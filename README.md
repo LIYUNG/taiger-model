f7161f1fe81ed63534e125e058c24e71fe0a9158
Leo <liyung.chen@tum.de>
2025-07-21 01:58:37 +0200

    fix: remove applications in students

--- README.md
+++ README.md
@@ -21,4 +21,4 @@ git push origin main --tags
 npm publish
 
 # Concat command for powershell (please modify the commit message)
-git add .; git commit -m "fix: copy applications to new collection"; git push origin main; npm run build; npm version patch; git push origin main --tags; npm publish
+git add .; git commit -m "fix: remove applications in students"; git push origin main; npm run build; npm version patch; git push origin main --tags; npm publish
--- src/model/User.ts
+++ src/model/User.ts
@@ -363,85 +363,11 @@ const userSchema = new Schema(
   options
 );
 
-const applicationSchema = new Schema({
-  programId: { type: Schema.Types.ObjectId, ref: 'Program' },
-  uni_assist: {
-    status: {
-      type: String,
-      default: 'notstarted'
-    },
-    vpd_file_path: {
-      type: String,
-      default: ''
-    },
-    vpd_paid_confirmation_file_path: {
-      type: String,
-      default: ''
-    },
-    vpd_paid_confirmation_file_status: {
-      type: String,
-      default: ''
-    },
-    isPaid: {
-      type: Boolean,
-      default: false
-    },
-    updatedAt: Date
-  },
-  portal_credentials: {
-    application_portal_a: {
-      account: { type: String, select: false, trim: true },
-      password: { type: String, select: false, trim: true }
-    },
-    application_portal_b: {
-      account: { type: String, select: false, trim: true },
-      password: { type: String, select: false, trim: true }
-    }
-  },
-  doc_modification_thread: [
-    {
-      isFinalVersion: {
-        type: Boolean,
-        default: false
-      },
-      latest_message_left_by_id: {
-        type: String,
-        default: ''
-      },
-      doc_thread_id: { type: Schema.Types.ObjectId, ref: 'Documentthread' },
-      updatedAt: Date,
-      createdAt: Date
-    }
-  ],
-  reject_reason: {
-    type: String,
-    default: ''
-  },
-  admission_letter: {
-    status: {
-      type: String,
-      default: 'notstarted'
-    },
-    admission_file_path: {
-      type: String,
-      default: ''
-    },
-    comments: { type: String, default: '' },
-    updatedAt: Date
-  },
-  finalEnrolment: { type: Boolean, default: false },
-  decided: { type: String, default: '-' },
-  closed: { type: String, default: '-' },
-  admission: { type: String, default: '-' },
-  application_year: { type: String, default: '' }
-});
-
 const studentSchema = new Schema(
   {
     agents: [{ type: Schema.Types.ObjectId, ref: 'Agent' }],
     editors: [{ type: Schema.Types.ObjectId, ref: 'Editor' }],
     needEditor: { type: Boolean, default: false },
-    applications: [applicationSchema],
     applying_program_count: {
       type: Number,
       default: 0
